f87fc13b1ca9c98c61b3f64b6081170d9ca8946a
Aminu Muhammad <aminuastroboy@gmail.com>
2026-05-18 12:08:59 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{padding: 40}}>
+      <h1>Hon. Ahmad Rufai Abdulhamid</h1>
+      <p>Official Campaign Website</p>
+    </main>
+  );
+}
